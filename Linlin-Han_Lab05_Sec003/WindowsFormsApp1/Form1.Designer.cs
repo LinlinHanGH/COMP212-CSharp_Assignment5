2490a09b7a9b3c1efa3c66443523cf288cc33e4c
Linlin <hanlinlin@live.com>
2018-07-26 15:49:24 -0400

    create WPF

---
+++ Linlin-Han_Lab05_Sec003/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenInt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenInt
+            // 
+            this.btnGenInt.Location = new System.Drawing.Point(12, 26);
+            this.btnGenInt.Name = "btnGenInt";
+            this.btnGenInt.Size = new System.Drawing.Size(217, 35);
+            this.btnGenInt.TabIndex = 0;
+            this.btnGenInt.Text = "Generate Random Int";
+            this.btnGenInt.UseVisualStyleBackColor = true;
+            this.btnGenInt.Click += new System.EventHandler(this.btnGenInt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 244);
+            this.Controls.Add(this.btnGenInt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenInt;
+    }
+}
+
---
+++ Linlin-Han_Lab05_Sec003/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenInt_Click(object sender, EventArgs e)
+        {
+            int Min = 0;
+            int Max = 20;
+            Random randNum = new Random();
+            int[] intArray = Enumerable
+                .Repeat(0, 5)
+                .Select(i => randNum.Next(Min, Max))
+                .ToArray();
+        }
+    }
+}
