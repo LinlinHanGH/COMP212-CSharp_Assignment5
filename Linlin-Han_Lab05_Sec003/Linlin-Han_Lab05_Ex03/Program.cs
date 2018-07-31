20b6c65c07734d612a5b92d0cbe56deb9f4301ce
Linlin <hanlinlin@live.com>
2018-07-31 16:28:59 -0400

    add search method

--- Linlin-Han_Lab05_Sec003/Linlin-Han_Lab05_Ex03/Program.cs
+++ Linlin-Han_Lab05_Sec003/Linlin-Han_Lab05_Ex03/Program.cs
@@ -13,9 +13,11 @@ namespace Linlin_Han_Lab05_Ex03
             string id;
             string name;
             Student student = new Student("123","Alice");
+            
             LinkedList<Student> lnkStudent = new LinkedList<Student>();
+            //AddLinkedListItem(lnkStudent, student);
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 2; i++)
             {
                 Console.WriteLine("Please enter the student ID:");
                 id = Console.ReadLine();
@@ -26,21 +28,21 @@ namespace Linlin_Han_Lab05_Ex03
             }
             Console.WriteLine("Print LinkedList after adding 5 students");
             PrintLinkedList(lnkStudent);
-            
 
-            Console.WriteLine();
-            Console.WriteLine("Please enter the student's ID that you want to remove");
-            id = Console.ReadLine();
-            Console.WriteLine("Please enter the student's Name that you want to remove");
-            name = Console.ReadLine();
-            Student rmStudent = new Student(id, name);
 
-            Console.WriteLine();
-            Console.WriteLine("Print LinkedList after removing 1 student");
-            RemoveLinkedListItem(lnkStudent, rmStudent);
-            PrintLinkedList(lnkStudent);
+            //Console.WriteLine();
+            //Console.WriteLine("Please enter the student's ID that you want to remove");
+            //id = Console.ReadLine();
+            //Console.WriteLine("Please enter the student's Name that you want to remove");
+            //name = Console.ReadLine();
+            //Student rmStudent = new Student(id, name);
 
-            Console.WriteLine();
+            //Console.WriteLine();
+            //RemoveLinkedListItem(lnkStudent, rmStudent);
+            //Console.WriteLine("Print LinkedList after removing 1 student");
+            //PrintLinkedList(lnkStudent);
+
+            //Console.WriteLine();
             Console.WriteLine("Please enter the student's ID that you want to search");
             id = Console.ReadLine();
             Console.WriteLine("Please enter the student's Name that you want to search");
@@ -52,58 +54,63 @@ namespace Linlin_Han_Lab05_Ex03
 
         }// end of main
 
-        public static void AddLinkedListItem<T>(LinkedList<T> lnk, T objectName)
+        public static void AddLinkedListItem(LinkedList<Student> lnkStudent, Student student)
         {
-            lnk.AddLast(objectName);
+            lnkStudent.AddLast(student);
         }
 
         public static void RemoveLinkedListItem(LinkedList<Student> lnkStudent, Student student)
         {
             LinkedListNode<Student> currentNode = lnkStudent.First;
+            bool result = false;
             while (currentNode != null)
             {
                 if (currentNode.Value.Id == student.Id && currentNode.Value.Name == student.Name)
                 {
                     lnkStudent.Remove(currentNode);
+                    result = true;
                 }
-                else
-                {
-                    Console.WriteLine("The student ID or name does not exist!");
-                }
+                
                 currentNode = currentNode.Next; // get next node             
             } // end while
+
+            if (result == false)
+            {
+                Console.WriteLine("The student ID or name does you want to remove not exist!");
+            }
         }
 
         public static int SearchLinkedListItem(LinkedList<Student> lnkStudent, Student student)
         {
             int index = 0;
+            bool result = false;
             LinkedListNode<Student> currentNode = lnkStudent.First;
             while (currentNode != null)
             {
                 if (currentNode.Value.Id == student.Id && currentNode.Value.Name == student.Name)
                 {
-                    lnkStudent.Remove(currentNode);
-                }
-                else
-                {
-                    Console.WriteLine("The student ID or name does not exist!");
-                    index = -1;
+                    result = true;
+                    return index;
                 }
                 currentNode = currentNode.Next; // get next node  
                 index++;
             } // end while
 
+            if (result==false) {
+                index = -1;
+            }
             return index;
         }
 
 
-        public static void PrintLinkedList<T>(LinkedList<T> lnk)
+        public static void PrintLinkedList(LinkedList<Student> lnkStudent)
         {
-            LinkedListNode<T> currentNode = lnk.First;
+            LinkedListNode<Student> currentNode = lnkStudent.First;
             while (currentNode != null)
             {
                 // get value in node       
-                Console.WriteLine($"\n Student ID: {currentNode.Value}"); 
+                Console.WriteLine($"\n Student ID: {currentNode.Value.Id}"); // convert to uppercase 
+                Console.WriteLine($" Student Name: {currentNode.Value.Name}");
                 currentNode = currentNode.Next; // get next node             
             } // end while
         }
