30b6455c96c968ef1007ac8775fba32d0c061d20
Linlin <hanlinlin@live.com>
2018-08-01 10:23:13 -0400

    ex4: add AddEmployee method()

--- Linlin-Han_Lab05_Sec003/Linlin-Han_Lab05_Ex04/Program.cs
+++ Linlin-Han_Lab05_Sec003/Linlin-Han_Lab05_Ex04/Program.cs
@@ -8,8 +8,27 @@ namespace Linlin_Han_Lab05_Ex04
 {
     class Program
     {
+        private static int key = 0;
         static void Main(string[] args)
         {
+            Employee[] employees = new[] {new Employee("Alice", 1000),
+                                        new Employee("Bill", 1200),
+                                        new Employee("Ella", 1000),
+                                        new Employee("Vivian", 800),
+                                        new Employee("David", 900)
+                                        };
+
+            SortedDictionary<int, Employee> employeeDict = new SortedDictionary<int, Employee>();
+
+            for (int i = 0; i < employees.Length; ++i)
+            {
+                AddDictionaryItem(employeeDict, employees[i]);
+            }
+        }
+
+        public static void AddDictionaryItem(SortedDictionary<int, Employee> employeeDict, Employee emp)
+        {
+            employeeDict.Add(++key, emp);
         }
     }
 }
